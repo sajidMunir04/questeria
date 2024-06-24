11a2a388b059d7d173284f124e6b27b924084801
SAJID <sajid.munir4428@hotmail.com>
2024-06-24 19:51:39 +0500

    Update NavSection.tsx

--- src/components/common/NavSection.tsx
+++ src/components/common/NavSection.tsx
@@ -9,11 +9,11 @@ interface Props {
 
 
 function NavSection(props : Props)  {
-    return (<div className="flex items-center flex-col justify-start bg-purple-background h-full w-2/12">
+    return (<div className="flex flex-col bg-purple-background h-full w-2/12 items-end">
         <div className='h-1/10 overflow-hidden'>
             <img className='m-auto max-w-full' src="/images/common/logo.png"/>
         </div>
-        <nav className='flex flex-col w-4/6 mr-0'>
+        <nav className='flex flex-col w-5/6 mr-0'>
             <button type='button' className={props.selectedView === SelectedView.Dashboard ? 'border-none font-poppinsMedium text-custom-color-one rounded-l-full w-full' : 'border-none rounded-none font-poppinsMedium bg-transparent text-custom-color-one'} onClick={props.onDashboardButtonClick}>Dashboard</button>
             <button type='button' className={props.selectedView === SelectedView.Quizzes ? 'border-none font-poppinsMedium text-custom-color-one rounded-l-full w-full' : 'border-none rounded-none font-poppinsMedium bg-transparent text-custom-color-one'} onClick={props.onQuizzesButtonClick}>My Quizzes</button>
             <button type='button' className={props.selectedView === SelectedView.CreateQuiz ? 'border-none font-poppinsMedium text-custom-color-one rounded-l-full w-full' : 'border-none rounded-none font-poppinsMedium bg-transparent text-custom-color-one'} onClick={props.onCreateQuizButtonClick}>Create Quiz</button>
