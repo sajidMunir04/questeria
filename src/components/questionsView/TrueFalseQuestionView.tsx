2339d7d8e7eb7b412d930732d9d9730ac1dc060a
SAJID <sajid.munir4428@hotmail.com>
2024-07-05 15:09:50 +0500

    Update TrueFalseQuestionView.tsx

--- src/components/questionsView/TrueFalseQuestionView.tsx
+++ src/components/questionsView/TrueFalseQuestionView.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import QuestionText from "./questionsComponents/QuestionText";
 
 
@@ -8,16 +9,20 @@ interface Props {
 
 function TrueFalseQuestionView(props : Props) {
 
+    const [answerState,setAnswerState] = useState<AnswerState>(AnswerState.NotSelected);
+
     return (<div>
         <QuestionText questionText={props.questionText}/>
         <div>
-        <div className='flex mb-3 items-center'>
+        <div className={`flex mb-3 items-center p-2 ${answerState === AnswerState.AnswerIsTrue && 'border-4 border-black rounded-xl'}`} onClick={() => {
+            setAnswerState(AnswerState.AnswerIsTrue);
+        }}>
             <div className='ml-2 rounded-full border-solid border-2 w-8 h-8 flex border-black'>
             <p className='m-auto'>&#10004;</p>
             </div>
             <p className='ml-5'>True</p>
         </div>
-        <div className={`flex mb-3 items-center`}>
+        <div className={`flex mb-3 items-center p-2 ${answerState === AnswerState.AnswerIsFalse && 'border-4 border-black rounded-xl'}`} onClick={()=>{setAnswerState(AnswerState.AnswerIsFalse)}}>
             <div className='ml-2 rounded-full  border-solid border-2 w-8 h-8 flex border-black'>
             <p className='m-auto'>&#10004;</p>
             </div>
@@ -25,6 +30,13 @@ function TrueFalseQuestionView(props : Props) {
         </div>
         </div>
     </div>);
+}
+
+
+const enum AnswerState {
+    NotSelected,
+    AnswerIsTrue,
+    AnswerIsFalse
 }
 
 export default TrueFalseQuestionView;
