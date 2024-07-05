3e2985b2027d047591f69e9389268f7a0b2c5c20
SAJID <sajid.munir4428@hotmail.com>
2024-07-05 15:24:41 +0500

    added blank area component

--- src/components/questionsView/SelectOptionsQuestionView.tsx
+++ src/components/questionsView/SelectOptionsQuestionView.tsx
@@ -1,3 +1,5 @@
+import { blankArea } from "../../lib/constants";
+import BlankArea from "./questionsComponents/BlankArea";
 import QuestionText from "./questionsComponents/QuestionText";
 
 interface Props {
@@ -9,7 +11,14 @@ interface Props {
 function SelectOptionsQuestionView(props : Props) {
     return (<div>
         <div>
-            {props.questionSections.map((item) => <QuestionText questionText={item}/>)}
+            {props.questionSections.map(function(item,index) {
+                if (item === blankArea){
+                    return <BlankArea/>
+                }
+                else {
+                    <QuestionText questionText={item}/>
+                }
+            })}
         </div>
     </div>);
 }
---
+++ src/components/questionsView/questionsComponents/BlankArea.tsx
@@ -0,0 +1,7 @@
+
+
+function BlankArea() {
+    return (<div className='w-20ch border-black border-b-2'></div>);
+}
+
+export default BlankArea;
